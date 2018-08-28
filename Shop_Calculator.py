b56a61b9ceff0b8c7a820d7d23159ca4003e64f1
jc496578 <cassandra.king4@my.jcu.edu.au>
2018-08-28 14:52:15 +1000

    Hello World

---
+++ Shop_Calculator.py
@@ -0,0 +1,11 @@
+#Number of items: 3
+#Price of item: 100
+#Price of item: 35.56
+#Price of item: 3.24
+#Total price for 3 items is $124.92
+
+itemnumber = input("How Many Items; ")
+itemPrice = input("Item Price: ")
+itemPrice1 = input("Item Price: ")
+total = itemPrice + itemPrice1
+print("Total price: $ {:.2f}".format(total))
---
+++ loops.py
@@ -0,0 +1,18 @@
+
+
+for i in range(1, 21, 2):
+    print(i, end=' ')
+print()
+
+for i in range(10, 110, 10):
+    print(i, end=' ')
+print()
+for i in reversed(range(21)):
+     print(i, end=' ')
+print()
+
+#number= input("How many Stars: ")
+#if number== 4;
+for i in range(0, 5):
+    print("*" * i)
+print()
